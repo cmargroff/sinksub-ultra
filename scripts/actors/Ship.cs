c6dec955f32d03f041780a3b095077f2a6e5a20e
Chris Margroff <sora3087@gmail.com>
2025-12-16 10:07:14 -0500

    bound ship to edges of camera

--- scripts/actors/Ship.cs
+++ scripts/actors/Ship.cs
@@ -9,14 +9,20 @@ public partial class Ship : Node3D
   [Export(PropertyHint.Range, "0,1,0.01")] public float FrictionCoefficient = 0.98f;
   private float _speed = 0f;
   private Mover _mover;
+  private Camera3D _camera;
+  private Marker3D _leftSensor;
+  private Marker3D _rightSensor;
 
   public override void _Ready()
   {
-    _mover = GetNode<Mover>("Mover");
+    _camera = GetTree().Root.GetCamera3D();
+    _mover = GetNode<Mover>("%Mover");
     if (_mover == null)
     {
       GD.PrintErr("Ship: Mover node not found!");
     }
+    _leftSensor = GetNode<Marker3D>("%SensorLeft");
+    _rightSensor = GetNode<Marker3D>("%SensorRight");
   }
 
   public override void _PhysicsProcess(double delta)
@@ -39,6 +45,27 @@ public partial class Ship : Node3D
     if (Input.IsActionJustPressed("launch_right"))
     {
 
+    }
+    CheckSensors();
+  }
+
+  private void CheckSensors()
+  {
+    if (!_camera.IsPositionInFrustum(_leftSensor.GlobalPosition))
+    {
+      if (_speed < 0)
+      {
+        _speed = 0;
+        _mover.SpeedX = 0;
+      }
+    }
+    else if (!_camera.IsPositionInFrustum(_rightSensor.GlobalPosition))
+    {
+      if (_speed > 0)
+      {
+        _speed = 0;
+        _mover.SpeedX = 0;
+      }
     }
   }
 
